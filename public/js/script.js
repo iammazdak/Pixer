a032a8e30adda28e6b8ec84a6e578c0c11fdd922
Mazdak <Mazdak.es23@gmail.com>
2023-10-02 16:52:24 +0330

    topbar added

---
+++ public/js/script.js
@@ -0,0 +1,12 @@
+// let document = $
+
+let topbar = document.getElementById('topbar');
+let closeBtn = document.getElementById('topbar-close-btn');
+
+closeBtn.addEventListener('click', function() {
+    topbar.style.display = "none"
+})
+
+
+
+console.log(closeBtn)
