03b1641d785e8d506e42f4dcb6df512018ec1e52
Mazdak <Mazdak.es23@gmail.com>
2023-10-02 16:50:38 +0330

    custom styles added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./public/**/*.html"],
   theme: {
     extend: {},
+    colors: {
+      primary: '#67754B',
+      secondary: '#DDE8BB',
+      lightgray: '#F6F6F3',
+      lightgray: '#F6F6F3',
+      balck: '#040406',
+    }
   },
   plugins: [],
 };
